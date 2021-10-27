dd53b1964cfce9f3b2bac794546e6c84e38bd31f
AhitkinIlya <43231077+AhitkinIlya@users.noreply.github.com>
2021-10-27 16:04:11 +0300

    Add files via upload

---
+++ frontend/data.js
@@ -0,0 +1,92 @@
+module.exports = {
+    "comments": {
+        "popular": [
+            {
+                id: 1,
+                user: {
+                    id: 1,
+                    fullname: 'Качек Володя',
+                    avatarUrl: 'https://sun9-31.userapi.com/impg/pSpPka9PTroqI_aYoQqAr1qAovXI7GAEh_SI2Q/4FxGmI6wVFA.jpg?size=757x757&quality=96&sign=6ca97a6cd1e8eae6e2d84151bd0a3d51&type=album'
+                },
+                text: 'Люблю Илью, маму, Полину... Ну и анаболики',
+                post: {
+                    id: 1,
+                    title: 'Моя история про то, как я не смог разнести по фактам свою девушку',
+                },
+                createdAt: new Date().toString()
+            },
+            {
+                id: 2,
+                user: {
+                    id: 1,
+                    fullname: 'Бородатый священник',
+                    avatarUrl: 'https://sun9-42.userapi.com/impg/FWKJdnfg8i2jxBnLGxc2gCwsvTzuwrRRxRH3ow/twPO8tmB1ts.jpg?size=1920x1440&quality=96&sign=dcd5c8a4d9313e37d907d9b530d7c1d6&type=album'
+                },
+                text: 'Не вижу смысла соглашаться в чем-либо с Ильей',
+                post: {
+                    id: 1,
+                    title: 'Как из отличника я превратился в распиздяя',
+                },
+                createdAt: new Date().toString()
+            },
+            {
+                id: 3,
+                user: {
+                    id: 1,
+                    fullname: 'Бородатый потеряшка',
+                    avatarUrl: 'https://sun9-19.userapi.com/impf/c836122/v836122566/2bd23/V1QJkCInwFA.jpg?size=540x960&quality=96&sign=f2b82419904c6c0311d85b18413fc056&type=album'
+                },
+                text: 'Обычно теряюсь, как чувства твоих девушек',
+                post: {
+                    id: 1,
+                    title: 'Как я не справился с алкоголем...Спойлер: и это не впервые.',
+                },
+                createdAt: new Date().toString()
+            },
+            {
+                id: 4,
+                user: {
+                    id: 1,
+                    fullname: 'Масюк Евгенич',
+                    avatarUrl: 'https://sun9-78.userapi.com/impg/PFwtiVBwKhm4RoZZ_20xesd0LqzTZPgYsuWsow/EQb_GlEfxCI.jpg?size=958x958&quality=96&sign=609dd86336fb952fda9a2d939ea22709&type=album'
+                },
+                text: 'В любой непонятной ситуации обвиняю Илью в наебаторстве',
+                post: {
+                    id: 1,
+                    title: 'Как держать мотивацию в развитии, после неудачного опыта в становлении киберспортсменом',
+                },
+                createdAt: new Date().toString()
+            }
+        ],
+        "new": [            
+            {
+                id: 2,
+                user: {
+                    id: 1,
+                    fullname: 'Бородатый священник',
+                    avatarUrl: 'https://sun9-42.userapi.com/impg/FWKJdnfg8i2jxBnLGxc2gCwsvTzuwrRRxRH3ow/twPO8tmB1ts.jpg?size=1920x1440&quality=96&sign=dcd5c8a4d9313e37d907d9b530d7c1d6&type=album'
+                },
+                text: 'Не вижу смысл в жизни и соглашаться в чем-либо с Ильей',
+                post: {
+                    id: 1,
+                    title: 'Какая у вас дома ванна?',
+                },
+                createdAt: new Date().toString()
+            },
+            {
+                id: 3,
+                user: {
+                    id: 1,
+                    fullname: 'Бородатый потеряшка',
+                    avatarUrl: 'https://sun9-19.userapi.com/impf/c836122/v836122566/2bd23/V1QJkCInwFA.jpg?size=540x960&quality=96&sign=f2b82419904c6c0311d85b18413fc056&type=album'
+                },
+                text: 'Обычно теряюсь, как чувства твоих девушек',
+                post: {
+                    id: 1,
+                    title: 'Какая у вас дома ванна?',
+                },
+                createdAt: new Date().toString()
+            }
+        ]
+    }
+}
